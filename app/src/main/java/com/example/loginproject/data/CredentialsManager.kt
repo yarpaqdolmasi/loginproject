666c9442afaa90714d50ea7cf068e46f37d9c80e
yarpaqdolmasi <ul0270230@edu.uni.lodz.pl>
2025-01-03 16:14:36 +0400

    Implement logic of Login and Register with credentials manager

--- app/src/main/java/com/example/loginproject/data/CredentialsManager.kt
+++ app/src/main/java/com/example/loginproject/data/CredentialsManager.kt
@@ -1,6 +1,10 @@
 package com.example.loginproject.data
 
+import android.content.SharedPreferences
+import android.util.Log
+
 class CredentialsManager {
+
     fun isEmailValid(email: String): Boolean {
         val isEmailValid = when {
             email.isNotEmpty() && email.matches(Regex("[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}")) -> true
@@ -15,5 +19,23 @@ class CredentialsManager {
             else -> false
         }
         return isPasswordValid
+    }
+
+    fun register(emailAndPassword: User, sharedPreferences: SharedPreferences): Boolean {
+        Log.d("testing", "register: ${emailAndPassword.email}")
+        if (sharedPreferences.getString(emailAndPassword.email, null).isNullOrEmpty()) {
+            sharedPreferences.edit().putString(emailAndPassword.email, emailAndPassword.password)
+                .apply()
+            return true
+        } else {
+            return false
+        }
+    }
+
+    fun login(emailAndPassword: User, sharedPreferences: SharedPreferences): Boolean {
+        val isPasswordCorrect = sharedPreferences.getString(emailAndPassword.email, null)?.let {
+            it == emailAndPassword.password
+        } ?: false
+        return isPasswordCorrect
     }
 }
